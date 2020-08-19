9222c3c2bff8ff352e18e29c234bddfce1671f85
luoxuhao <rossihox@gmail.com>
2020-08-20 00:13:45 +0800

    [ADD] read latency cdf

--- coremem_latency/mem_latency_cdf.py
+++ coremem_latency/mem_latency_cdf.py
@@ -8,11 +8,12 @@ def gen_cdf(filename):
     with open(filename, 'r') as fd:
         lines = fd.read().splitlines()
 
+    cycle_time = 4  # 4ns per cycle
     # skip none integer lines
     data = []
     for line in lines:
         try:
-            data.append(int(line))
+            data.append(int(line) * cycle_time)
         except ValueError:
             pass
 
@@ -26,24 +27,28 @@ def gen_cdf(filename):
     return [x, y]
 
 
-def draw_cdf(pgf_trace, tlb_hit_trace, tlb_miss_trace, saveto="mem_latency.jpg"):
+def draw_cdf(pgf_trace, tlb_hit_read_trace, tlb_hit_write_trace, tlb_miss_read_trace, tlb_miss_write_trace, saveto="mem_latency.jpg"):
     pgf_cdf = gen_cdf(pgf_trace)
-    tlb_hit_cdf = gen_cdf(tlb_hit_trace)
-    tlb_miss_cdf = gen_cdf(tlb_miss_trace)
+    tlb_hit_read_cdf = gen_cdf(tlb_hit_read_trace)
+    tlb_hit_write_cdf = gen_cdf(tlb_hit_write_trace)
+    tlb_miss_read_cdf = gen_cdf(tlb_miss_read_trace)
+    tlb_miss_write_cdf = gen_cdf(tlb_miss_write_trace)
 
     plt.figure(constrained_layout=True)
     plt.rcParams.update({'font.size': 16})
     plt.title("Core Memory Access Latency", pad=7.0)
-    plt.xlabel('Latency (Clock Cycle)')
+    plt.xlabel('Latency (ns)')
     plt.ylabel('Percentile')
 
-    plt.plot(*pgf_cdf, lw=3, label="Page Fault")
-    plt.plot(*tlb_hit_cdf, lw=3, label="TLB Hit")
-    plt.plot(*tlb_miss_cdf, lw=3, label="TLB Miss, \nw/o Page Fault")
+    plt.plot(*pgf_cdf, lw=3, label="Page Fault (write)")
+    plt.plot(*tlb_hit_read_cdf, lw=3, label="TLB Hit (read)")
+    plt.plot(*tlb_hit_write_cdf, lw=3, label="TLB Hit (write)")
+    plt.plot(*tlb_miss_read_cdf, lw=3, label="TLB Miss (read), \nw/o Page Fault")
+    plt.plot(*tlb_miss_write_cdf, lw=3, label="TLB Miss (write), \nw/o Page Fault")
     plt.grid()
     plt.legend()
     plt.savefig(saveto)
 
 
 if __name__ == "__main__":
-    draw_cdf("data_used/lat_pgf_64_w.txt", "data_used/lat_64_w.txt", "data_used/lat_64_miss_w.txt")
+    draw_cdf("data_used/lat_pgf_64_w.txt", "data_used/lat_64_r.txt", "data_used/lat_64_w.txt","data_used/lat_64_miss_r.txt", "data_used/lat_64_miss_w.txt")
